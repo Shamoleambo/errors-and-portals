889344f89c6f036e09fe0c88249582130593c16c
Tiago Gomes <tidgomes@gmail.com>
2023-07-05 17:05:14 -0300

    reset name and age after submitting the form

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -8,28 +8,32 @@ const UserForm = ({ addUser }) => {
     event.preventDefault()
     const id = Math.floor(Math.random() * 1000)
     addUser({ id, name, age })
+    setName('')
+    setAge('')
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className='userForm' onSubmit={handleSubmit}>
       <h1>Add User</h1>
-      <div className='form-group'>
-        <label htmlFor='name'>Name</label>
-        <input
-          type='text'
-          id='name'
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-      </div>
-      <div className='form-group'>
-        <label htmlFor='age'>Age</label>
-        <input
-          type='number'
-          id='age'
-          value={age}
-          onChange={(event) => setAge(event.target.value)}
-        />
+      <div className='form-fields'>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input
+            type='text'
+            id='name'
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='age'>Age</label>
+          <input
+            type='number'
+            id='age'
+            value={age}
+            onChange={(event) => setAge(event.target.value)}
+          />
+        </div>
       </div>
       <button type='submit'>Add User</button>
     </form>
