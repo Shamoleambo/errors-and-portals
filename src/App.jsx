bf091adb0688ab76d310f96062825ed70a6fc0ff
Tiago Gomes <tidgomes@gmail.com>
2023-07-10 14:29:19 -0300

    feat: display ErrorModal in case of error

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { useState } from 'react'
 import UserForm from './components/UserForm'
 import UsersList from './components/UsersList'
+import ErrorModal from './modals/ErrorModal'
 import './App.css'
 
 function App() {
   const [usersList, setUsersList] = useState([])
+  const [error, setError] = useState(false)
+
+  const handleError = () => {
+    setError(false)
+  }
 
   const addUser = (user) => {
+    const nameIsEmpty = !user.name.trim()
+    const ageIsEmpty = !user.age.trim()
+
+    if (nameIsEmpty || ageIsEmpty) {
+      setError(true)
+      return
+    }
+
     setUsersList((prevState) => [...prevState, user])
   }
   return (
     <div className='app'>
+      {error && <ErrorModal onConfirm={handleError} />}
       <UserForm addUser={addUser} />
       <UsersList usersList={usersList} />
     </div>
