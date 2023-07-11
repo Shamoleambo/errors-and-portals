03b8a5317d8c2d7572bd7b2910d80716b3d6cf1f
Tiago Gomes <tidgomes@gmail.com>
2023-07-11 13:40:29 -0300

    refactor: apply useRef instead of useState to retrieve the value provided in the inputs

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 import './UserForm.css'
 
 const UserForm = ({ addUser }) => {
-  const [name, setName] = useState('')
-  const [age, setAge] = useState('')
+  const nameInput = useRef()
+  const ageInput = useRef()
 
   const handleSubmit = (event) => {
     event.preventDefault()
+    const name = nameInput.current.value
+    const age = ageInput.current.value
     const id = Math.floor(Math.random() * 1000)
+
     addUser({ id, name, age })
-    setName('')
-    setAge('')
+    nameInput.current.value = ''
+    ageInput.current.value = ''
   }
 
   return (
@@ -19,21 +22,11 @@ const UserForm = ({ addUser }) => {
       <div className='form-fields'>
         <div className='form-group'>
           <label htmlFor='name'>Name</label>
-          <input
-            type='text'
-            id='name'
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-          />
+          <input type='text' id='name' ref={nameInput} />
         </div>
         <div className='form-group'>
           <label htmlFor='age'>Age</label>
-          <input
-            type='number'
-            id='age'
-            value={age}
-            onChange={(event) => setAge(event.target.value)}
-          />
+          <input type='number' id='age' ref={ageInput} />
         </div>
       </div>
       <button type='submit'>Add User</button>
