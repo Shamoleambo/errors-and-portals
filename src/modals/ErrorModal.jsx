e8f4c385bbcca5bc4932e1892253f645abecea40
Tiago Gomes <tidgomes@gmail.com>
2023-07-11 13:29:19 -0300

    refactor: create a portal to render the backdrop and modal overlays

--- src/modals/ErrorModal.jsx
+++ src/modals/ErrorModal.jsx
@@ -1,13 +1,30 @@
+import { createPortal } from 'react-dom'
 import './ErrorModal.css'
+
+const Backdrop = ({ onConfirm }) => {
+  return <div className='backdrop' onClick={onConfirm}></div>
+}
+
+const OverlayModal = ({ onConfirm }) => {
+  return (
+    <div className='errorModal'>
+      <p className='message'>Name and Age must not be empty!</p>
+      <button onClick={onConfirm}>OK</button>
+    </div>
+  )
+}
 
 const ErrorModal = ({ onConfirm }) => {
   return (
     <>
-      <div className='backdrop' onClick={onConfirm}></div>
-      <div className='errorModal'>
-        <p className='message'>Name and Age must not be empty!</p>
-        <button onClick={onConfirm}>OK</button>
-      </div>
+      {createPortal(
+        <Backdrop onConfirm={onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+      {createPortal(
+        <OverlayModal onConfirm={onConfirm} />,
+        document.getElementById('overlay-root')
+      )}
     </>
   )
 }
